8c2291a8108fab3aa4f3c4c170f748c989ecfe7d
Nick - Work <Nick.Koelble@gmail.com>
2024-09-04 18:54:11 -0400

    Minor changes and added comments

--- api.py
+++ api.py
@@ -4,6 +4,19 @@ from espn_api.football import League
 
 load_dotenv()
 
+"""
+Returns a league instance for a specified year.
+
+Parameters: 
+    year (int): selects the year of data to get. Defaults to current year
+"""
+def get_league(year=2024):
+    return League(league_id=os.getenv("LEAGUE_ID"), year=year, espn_s2=os.getenv("ESPN_S2"), swid=os.getenv("SWID"))
+
+league = get_league() # Create private leauge instance for use in other functions
+
+"""
+Returns name of league
+"""
 def get_league_name():
-    league = League(league_id=os.getenv("LEAGUE_ID"), year=2024, espn_s2=os.getenv("ESPN_S2"), swid=os.getenv("SWID"))
     return league.settings.name
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from api import get_league_name
+from api import *
 
 app = Flask(__name__)
 
@@ -8,8 +8,7 @@ def home():
     return render_template(
         'home.html',
         name=get_league_name()
-        )
+    )
 
-#py -m pip install dotenv
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
